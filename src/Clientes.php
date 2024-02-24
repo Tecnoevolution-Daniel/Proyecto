5305de6e3c6279d47f570724efdce620bd2d9621
DanielRestrepo88 <drestrepo@brickellfintech.com>
2024-02-23 20:32:03 -0500

    Continuacion del html

--- src/Clientes.php
+++ src/Clientes.php
@@ -50,7 +50,7 @@
                                 <input type="text" class="form-control" id="nombreRazonSocial" name="nombreRazonSocial" required>
                             </div>
                             <div class="form-group col-md-4">
-                                <label for="nit">NIT:</label>
+                                <label for="nit">NIT/CC/CE/PA (PN o PJ):</label>
                                 <input type="text" class="form-control" id="nit" name="nit" required>
                             </div>
                             <div class="form-group col-md-4">
@@ -209,15 +209,128 @@
                     <div class="card-body">
                         <div class="form-row">
                             <div class="form-group col-md-4">
-
+                                <label for="nombresApellidos"> Nombres y apellidos </label>
+                                <input type="text" class="form-control" id="nombresApellidos" name="nombresApellidos" required>
                             </div>
                             <div class="form-group col-md-4">
-
+                                <label for="numIdentificacion"> No.Identificacion </label>
+                                <input type="text" class="form-control" id="numIdentificacion" name="numIdentificacion" required>
                             </div>
                             <div class="form-group col-md-4">
-
+                                <label for="tipoIdentificacion">Tipo:</label>
+                                <select id="tipoIdentificacion" class="form-control" name="tipoIdentificacion" required>
+                                    <option selected disabled value="">Selecciona...</option>
+                                    <option>Cedula de ciudadania</option>
+                                    <option>Cedula de extranjeria</option>
+                                    <option>Pasaporte</option>
+                                    <option>Otro</option>
+                                </select>
                             </div>
                         </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-4">
+                                <label for="fechaExpedicion">Fecha de expedicion:</label>
+                                <input type="text" class="form-control" id="fechaExpedicion" name="fechaExpedicion" required>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="lugarExpedicion">Lugar de Expedición:</label>
+                                <input type="text" class="form-control" id="lugarExpedicion" name="lugarExpedicion" required>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="lugarNacimientoCiudad">Ciudad:</label>
+                                <input type="text" class="form-control" id="lugarNacimientoCiudad" name="lugarNacimientoCiudad" required>
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-4">                           
+                                <label for="fechaNacimiento">Fecha de Nacimiento:</label>
+                                <input type="date" class="form-control" id="fechaNacimiento" name="fechaNacimiento" required>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="nacionalidad">Lugar de Nacimiento:</label>
+                                <input type="text" class="form-control" id="nacionalidad" name="nacionalidad" required>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="profesion">Profesion:</label>
+                                <input type="text" class="form-control" id="profesion" name="profesion" required>
+                            </div>   
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-4">
+                                <label for="accionista">Es accionista de la empresa:</label>
+                                <select id="accionista" class="form-control" name="accionista" required>
+                                    <option selected disabled value="">Selecciona...</option>
+                                    <option>Si</option>
+                                    <option>No</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="correoElectronico">Correo Electrónico:</label>
+                                <input type="email" class="form-control" id="correoElectronico" name="correoElectronico" required>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="numContacto">Numero de contacto:</label>
+                                <input type="text" class="form-control" id="numContacto" name="numContacto" required>
+                            </div>   
+                        </div>
+                    </div>
+            </div>
+
+            <div class="card mt-4">
+                    <div class="card-header">
+                        INFORMACION SOBRE LA ACTIVIDAD ECONOMICA DE LA EMPRESA O PERSONA NATURAL
+                    </div>
+                    <div class="card-body">
+                        <div class="form-row">
+                            <div class="form-group col-md-4">
+                                <label for="codigo"> Codigo CIIU </label>
+                                <input type="text" class="form-control" id="codigo" name="codigo" required>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="lineaProductos">Linea de productos y servicios(Descripcion) </label>
+                                <input type="text" class="form-control" id="lineaProductos" name="lineaProductos">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="actividad">Actividad:</label>
+                                <select id="actividad" class="form-control" name="actividad" required>
+                                    <option selected disabled value="">Selecciona...</option>
+                                    <option>Productos/Bienes</option>
+                                    <option>Servicios</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-4">
+                                <label for="especialidad">Especialidad:</label>
+                                <select id="especialidad" class="form-control" name="especialidad" required>
+                                    <option selected disabled value="">Selecciona...</option>
+                                    <option>Fabricante</option>
+                                    <option>Importador</option>
+                                    <option>Intermediario</option>
+                                    <option>Distribuidor</option>
+                                    <option>Outsourcing</option>
+                                    <option>Servicios</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="informacionTributaria">Especialidad:</label>
+                                <select id="informacionTributaria" class="form-control" name="informacionTributaria" required>
+                                    <option selected disabled value="">Selecciona...</option>
+                                    <option>Gran contribuyente</option>
+                                    <option>Autorretenedor</option>
+                                    <option>Regimen comun</option>
+                                    <option>Regimen simplificado</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="resolucion">No. Resolucion</label>
+                                <input type="text" class="form-control" id="resolucion" name="resolucion" required>
+                            </div>
+                        </div>
+
 
                         <div class="form-row">
                             <div class="form-group col-md-4">
@@ -230,17 +343,6 @@
 
                             </div>
                         </div>
-
-
-
-
-
-
-
-
-
-
-
 
 
 
