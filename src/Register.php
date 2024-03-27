df76a847fc4dd70f304abb0c71254ae9aef0ef5c
DanielRestrepo88 <drestrepo@brickellfintech.com>
2024-03-26 22:16:33 -0500

    CSRF

--- src/Register.php
+++ src/Register.php
@@ -7,8 +7,21 @@ ob_start();
 // Conexion a la base de datos
 include 'Connection.php';
 
+// Generar y almacenar el token CSRF en la sesión
+if (!isset($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = password_hash(uniqid(), PASSWORD_DEFAULT);
+}
+
 // Campos a llenar en la base de datos
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Verificar el token CSRF
+    if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        // Token CSRF inválido
+        echo "Error de seguridad. Acceso no autorizado.";
+        exit();
+    }
+
     $Usuario = $_POST['usuario'];
     $Correo = $_POST['correo'];
     $Contrasena = $_POST['contrasena'];
@@ -16,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $Rol = $_POST['rol'];
 
      //Validacion donde no se repitan los correos
-     $query = "SELECT * FROM usuarios WHERE correo = :correo";
+     $query = "SELECT * FROM register WHERE correo = :correo";
      $stmt = $pdo->prepare($query);
      $stmt->execute(['correo' => $Correo]);
      if ($stmt->rowCount() > 0) {
@@ -44,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     // Agregar los datos a la Base de datos
-    $query = "INSERT INTO usuarios(usuario, correo, contrasena, repita_contrasena, rol) VALUES(:usuario, :correo, :contrasena, :repita_contrasena, :rol)";
+    $query = "INSERT INTO register(usuario, correo, contrasena, repita_contrasena, rol) VALUES(:usuario, :correo, :contrasena, :repita_contrasena, :rol)";
     $stmt = $pdo->prepare($query);
     $stmt->execute(['usuario' => $Usuario, 'correo' => $Correo, 'contrasena' => $hashedContrasena, 'repita_contrasena' => $RepitahashedContrasena, 'rol' => $Rol]);
 
@@ -72,7 +85,7 @@ ob_end_flush();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .form-container {
-            /*background-color: #d0d5db;*/
+            background-color: #d0d5db;
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -85,49 +98,46 @@ ob_end_flush();
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                            <h3 >Registro</h3>
-                        </div>
-                <div class="form-container">
-                    <form action="Register.php" method="post">
-                        <div class="form-group" >
-                            <label for="usuario">Usuario</label>
-                            <input type="text" class="form-control" id="usuario" name="usuario" required>
-                        </div>
+            <div class="form-container">
+                <form action="Register.php" method="post">
+                    <!-- Campo oculto para el token CSRF -->
+                    <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+                    <div class="form-group">
+                        <label for="usuario">Usuario:</label>
+                        <input type="text" class="form-control" id="usuario" name="usuario" required>
+                    </div>
 
-                        <div class="form-group">
-                            <label for="correo">Correo</label>
-                            <input type="email" class="form-control" id="correo" name="correo" required>
-                        </div>
+                    <div class="form-group">
+                        <label for="correo">Correo:</label>
+                        <input type="email" class="form-control" id="correo" name="correo" required>
+                    </div>
 
-                        <div class="form-group">
-                            <label for="contrasena">Contraseña</label>
-                            <input type="password" class="form-control" id="contrasena" name="contrasena" required>
-                        </div>
+                    <div class="form-group">
+                        <label for="contrasena">Contraseña:</label>
+                        <input type="password" class="form-control" id="contrasena" name="contrasena" required>
+                    </div>
 
-                        <div class="form-group">
-                            <label for="repita_contrasena">Repita Contraseña</label>
-                            <input type="password" class="form-control" id="repita_contrasena" name="repita_contrasena" required>
-                        </div>
+                    <div class="form-group">
+                        <label for="repita_contrasena">Repita Contraseña:</label>
+                        <input type="password" class="form-control" id="repita_contrasena" name="repita_contrasena" required>
+                    </div>
 
-                        <div class="form-group">
-                            <label for="rol">Rol</label>
-                            <select class="form-control" id="rol" name="rol" required>
-                                <option value="cliente">Cliente</option>
-                                <option value="proveedor">Proveedor</option>
-                                <option value="empleado">Empleado</option>
-                            </select>
-                        </div>
+                    <div class="form-group">
+                        <label for="rol">Rol:</label>
+                        <select class="form-control" id="rol" name="rol" required>
+                            <option value="cliente">Cliente</option>
+                            <option value="proveedor">Proveedor</option>
+                            <option value="empleado">Empleado</option>
+                        </select>
+                    </div>
 
-                        <button type="submit" class="btn btn-primary">Registrarse</button>
-                        <!-- Botón para volver a la página de inicio de sesión -->
-                        <div class="mt-3 text-center">
-                            <a href="Login.php" class="btn btn-link">Volver a Inicio de Sesión</a>
-                        </div>
-                    </form>
-                </div>
-            </div>  
+                    <button type="submit" class="btn btn-primary">Registrarse</button>
+                     <!-- Botón para volver a la página de inicio de sesión -->
+                    <div class="mt-3 text-center">
+                        <a href="Login.php" class="btn btn-link">Volver a Inicio de Sesión</a>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
--- src/cambiar_contrasena.php
+++ src/cambiar_contrasena.php
@@ -1,58 +1,12 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Olvidé mi contraseña</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-</head>
-<body>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header" style="">
-                        <h3 >Recuperar contraseña</h3>
-                    </div>
-                    <div class="card-body">
-                        <form action="cambiar_contrasena.php" method="POST">
-                            <div class="form-group">
-                                <label for="correo">Ingrese su correo electrónico</label>
-                                <div>
-                                    <input type="email" class="form-control input-short" style="text-align: center;" id="correo" name="correo" placeholder="Ejemplo@gmail.com" required>
-                                </div>
-                            </div>
-                            <div >
-                                <button type="submit" class="btn btn-primary btn-block" >Enviar</button>
-                            </div>
-                            
-                            
-                        </form>
-
-                        <div class="mt-3 text-center">
-                            <a href="Login.php" >Volver atrás</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</body>
-</html>
-
-
 <?php
-// Incluir la clase PHPMailer
+
+require_once __DIR__ . '/vendor/autoload.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 // Incluir el archivo de conexión a la base de datos
 include 'Connection.php';
-include 'vendor/autoload.php';
-
 
 // Verificar si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -61,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
 
     // Consultar la base de datos para verificar si el correo existe
-    $query = "SELECT * FROM usuarios WHERE correo = :correo";
+    $query = "SELECT * FROM register WHERE correo = :correo";
     $stmt = $pdo->prepare($query);
     $stmt->execute(['correo' => $Correo]);
 
@@ -126,3 +80,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olvidé mi contraseña</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header" style="">
+                        <h3 >Recuperar contraseña</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="cambiar_contrasena.php" method="POST">
+                            <div class="form-group">
+                                <label for="correo">Ingrese su correo electrónico</label>
+                                <div>
+                                    <input type="email" class="form-control input-short" style="text-align: center;" id="correo" name="correo" placeholder="Ejemplo@gmail.com" required>
+                                </div>
+                            </div>
+                            <div >
+                                <button type="submit" class="btn btn-primary btn-block" >Enviar</button>
+                            </div>
+                        </form>
+                        <div class="mt-3 text-center">
+                            <a href="Login.php" >Volver atrás</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
